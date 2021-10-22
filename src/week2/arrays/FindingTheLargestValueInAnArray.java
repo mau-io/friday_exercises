9686d23fee2f6a405519b214d48a36b85974f6e3
mau-io <maurice222@outlook.com>
2021-10-22 15:15:26 -0700

    Update array files

--- src/week2/arrays/FindingTheLargestValueInAnArray.java
+++ src/week2/arrays/FindingTheLargestValueInAnArray.java
@@ -1,22 +1,22 @@
 package week2.arrays;
 public class FindingTheLargestValueInAnArray {
 
-	public static void main( String[] args) {
-		int[] numbers = new int[10];
-		int maxNum = 0;
-		
-		System.out.print( "Array: " );
-		for(int i = 0; i < numbers.length; i++) {
-			numbers[i] = 1 + (int)(Math.random() * 100);
-			System.out.print( numbers[i] + " " );
-		}
-		
-		for(int i = 0; i < numbers.length; i++) {
-			if(maxNum < numbers[i]){
-				maxNum = numbers[i];
-			}
-		}
-		
-		System.out.println("\n\nThe largest value is " + maxNum);
-	}
+    public static void main( String[] args) {
+        int[] numbers = new int[10];
+        int maxNum = 0;
+
+        System.out.print( "Array: " );
+        for(int i = 0; i < numbers.length; i++) {
+            numbers[i] = 1 + (int)(Math.random() * 100);
+            System.out.print( numbers[i] + " " );
+        }
+
+        for(int i = 0; i < numbers.length; i++) {
+            if(maxNum < numbers[i]){
+                maxNum = numbers[i];
+            }
+        }
+
+        System.out.println("\n\nThe largest value is " + maxNum);
+    }
 }
--- src/week2/arrays/LocatingTheLargestValue.java
+++ src/week2/arrays/LocatingTheLargestValue.java
@@ -1,25 +1,25 @@
 package week2.arrays;
 public class LocatingTheLargestValue {
-	public static void main( String[] args) {
+    public static void main( String[] args) {
 
-		int[] numbers = new int[10];
-		int maxNum = 0;
-		int numAt = 0;
-		
-		System.out.print("Array: ");
-		for(int i = 0; i < numbers.length; i++) {
-			numbers[i] = 1 + (int)(Math.random() * 100);
-			System.out.print( numbers[i] + " " );
-		}
-		
-		for(int i = 0; i < numbers.length; i++) {
-			if(maxNum < numbers[i]) {
-				maxNum = numbers[i];
-				numAt = i;
-			}
-		}
-		
-		System.out.println("\n\nThe largest value is " + maxNum);
-		System.out.println("It is in slot " + numAt);
-	}
+        int[] numbers = new int[10];
+        int maxNum = 0;
+        int numAt = 0;
+
+        System.out.print("Array: ");
+        for(int i = 0; i < numbers.length; i++) {
+            numbers[i] = 1 + (int)(Math.random() * 100);
+            System.out.print( numbers[i] + " " );
+        }
+
+        for(int i = 0; i < numbers.length; i++) {
+            if(maxNum < numbers[i]) {
+                maxNum = numbers[i];
+                numAt = i;
+            }
+        }
+
+        System.out.println("\n\nThe largest value is " + maxNum);
+        System.out.println("It is in slot " + numAt);
+    }
 }
